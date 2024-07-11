d5e3638261500e834254cff2f740539bc80b6ada
callistoAshley <callistoashley@proton.me>
2024-07-11 10:50:49 +1000

    find vulkan package

--- src/main.c
+++ src/main.c
@@ -2,11 +2,25 @@
 #include <stdlib.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_vulkan.h>
+#include <vulkan/vulkan.h>
 
 int main(int argc, char *argv[])
 {
-    int vk_extensions;
+    VkInstance vulkan;
+    const VkInstanceCreateInfo inst_info =
+    {
+        VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        NULL,
+        0,
+        NULL,
+        0,
+        NULL,
+        0,
+        NULL
+    };
 
     SDL_Init(SDL_INIT_TIMER | SDL_INIT_VIDEO);
+    vkCreateInstance(&inst_info, NULL, &vulkan);
+    vkDestroyInstance(vulkan, NULL);
     SDL_Quit();
 }
