778dad4c955d8f53bde39f2a4af78ce74ec4115f
callistoAshley <callistoashley@proton.me>
2024-08-26 20:40:39 +1000

    remove test window

--- src/debug/windows/test.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "window.h"
-#include "utility/macros.h"
-
-extern Window test_window;
-
-void wnd_test_init(Window *self);
-void wnd_test_update(Window *self);
-void wnd_test_free(Window *self);
