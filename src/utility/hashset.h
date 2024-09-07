38fdb7804dcdc5c8851f8039ac0a22b3cfec8d01
Speak2Erase <lily@nowaffles.com>
2024-09-07 14:52:11 +0100

    add untested hashset

---
+++ src/utility/hashset.h
@@ -0,0 +1,46 @@
+#pragma once
+#include "sensible_nums.h"
+#include "utility/vec.h"
+#include <stdbool.h>
+
+typedef u64(hash_function)(void *key, usize key_size);
+typedef bool(eq_function)(void *a, void *b, usize key_size);
+
+// NOTE: keys are stored inline with the buckets.
+typedef struct
+{
+    u64 hash;
+    bool dead, occupied;
+    // this is a flexible array member, so it's not counted in the size of the
+    // struct.
+    char key[];
+} Bucket;
+
+// a growable set of keys that contains no duplicates.
+typedef struct
+{
+    u32 len;
+    u32 capacity;
+    usize key_size;
+
+    Bucket *buckets;
+
+    hash_function *hash;
+    eq_function *eq;
+} HashSet;
+
+u64 fnv_hash_function(void *key, usize key_size);
+bool memcmp_eq_function(void *a, void *b, usize key_size);
+
+void hashset_init(HashSet *set, hash_function *hash, eq_function *eq,
+                  usize key_size);
+void hashset_free(HashSet *set);
+
+// returns true if the key was inserted, false if it was already present.
+bool hashset_insert(HashSet *set, void *key);
+// returns true if the key was removed, false if it was not present.
+bool hashset_remove(HashSet *set, void *key);
+// returns true if the key is present in the set.
+bool hashset_contains(HashSet *set, void *key);
+
+void hashset_clear(HashSet *set);
