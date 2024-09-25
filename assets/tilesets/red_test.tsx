6c6e24acbe359ded8da3f5a85a2f1a7a48570b73
Melody Madeline Lyons <lily@nowaffles.com>
2024-09-25 13:41:59 -0700

    messing around with templates

---
+++ assets/tilesets/red_test.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="red_test" tilewidth="8" tileheight="8" tilecount="88" columns="8">
+ <grid orientation="orthogonal" width="0" height="0"/>
+ <image source="../textures/red_start.png" width="64" height="92"/>
+</tileset>
