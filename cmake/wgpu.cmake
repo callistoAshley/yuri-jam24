4397821accf2a54f3a07b4f78aec17e4f4445d1d
Melody Madeline Lyons <lily@nowaffles.com>
2024-09-29 11:09:42 -0700

    build wgpu-native with corrosion

---
+++ cmake/wgpu.cmake
@@ -0,0 +1,14 @@
+include(FetchContent)
+
+FetchContent_Declare(
+    Corrosion
+    GIT_REPOSITORY https://github.com/corrosion-rs/corrosion.git
+    GIT_TAG v0.5 # Optionally specify a commit hash, version tag or branch here
+)
+FetchContent_MakeAvailable(Corrosion)
+
+corrosion_import_crate(
+  MANIFEST_PATH ${CMAKE_SOURCE_DIR}/vendor/wgpu-native/Cargo.toml
+  CRATE_TYPES staticlib
+  FEATURES vulkan-portability
+)
