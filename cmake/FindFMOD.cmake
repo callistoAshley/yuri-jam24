db0c1a405a059ece2a1cc7962595785611d0fb5b
Speak2Erase <melody@melody-is.gay>
2024-10-12 23:48:32 +1100

    fix windows again

--- cmake/FindFMOD.cmake
+++ cmake/FindFMOD.cmake
@@ -1,28 +1,56 @@
 # FMOD is weird about libs so we have to search in multiple places
 
 # FMOD
-find_library(
+if (WIN32)
+  find_library(
     FMOD_LIBRARY 
     NAMES fmod fmod_vc
-    PATHS ${CMAKE_SOURCE_DIR}/vendor/fmod/api/core/lib/x86_64 ${CMAKE_SOURCE_DIR}/vendor/fmod/api/core/lib/x64
+    PATHS ${CMAKE_SOURCE_DIR}/vendor/fmod/api/core/lib/x64
     REQUIRED
-)
-find_path(
+  )
+  find_path(
     FMOD_INCLUDE_DIR
     NAMES fmod.h
     PATHS ${CMAKE_SOURCE_DIR}/vendor/fmod/api/core/inc 
     REQUIRED
-)
-# FMOD Studio
-find_library(
+  )
+  # FMOD Studio
+  find_library(
     FMOD_STUDIO_LIBRARY 
     NAMES fmodstudio fmodstudio_vc
-    PATHS ${CMAKE_SOURCE_DIR}/vendor/fmod/api/studio/lib/x86_64 ${CMAKE_SOURCE_DIR}/vendor/fmod/api/studio/lib/x64
+    PATHS ${CMAKE_SOURCE_DIR}/vendor/fmod/api/studio/lib/x64
     REQUIRED
-)
-find_path(
+  )
+  find_path(
     FMOD_STUDIO_INCLUDE_DIR
     NAMES fmod_studio.h
     PATHS ${CMAKE_SOURCE_DIR}/vendor/fmod/api/studio/inc 
     REQUIRED
-)
+  )
+else()
+  find_library(
+      FMOD_LIBRARY 
+      NAMES fmod
+      PATHS ${CMAKE_SOURCE_DIR}/vendor/fmod/api/core/lib/x86_64
+      REQUIRED
+  )
+  find_path(
+      FMOD_INCLUDE_DIR
+      NAMES fmod.h
+      PATHS ${CMAKE_SOURCE_DIR}/vendor/fmod/api/core/inc 
+      REQUIRED
+  )
+  # FMOD Studio
+  find_library(
+      FMOD_STUDIO_LIBRARY 
+      NAMES fmodstudio
+      PATHS ${CMAKE_SOURCE_DIR}/vendor/fmod/api/studio/lib/x86_64
+      REQUIRED
+  )
+  find_path(
+      FMOD_STUDIO_INCLUDE_DIR
+      NAMES fmod_studio.h
+      PATHS ${CMAKE_SOURCE_DIR}/vendor/fmod/api/studio/inc 
+      REQUIRED
+  )
+endif()
