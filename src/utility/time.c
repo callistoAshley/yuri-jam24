925a4f5954c320a06d2ae93d0cc2c79fcdd4f605
Melody Madeline Lyons <melody@melody-is.gay>
2024-10-18 05:26:05 -0700

    add time utility

---
+++ src/utility/time.c
@@ -0,0 +1,50 @@
+#include "time.h"
+#include "SDL3/SDL_timer.h"
+
+Duration duration_new(u64 nanos) { return (Duration){nanos}; }
+Duration duration_from_secs(u64 secs)
+{
+    return (Duration){SDL_SECONDS_TO_NS(secs)};
+}
+Duration duration_from_millis(u64 millis)
+{
+    return (Duration){SDL_MS_TO_NS(millis)};
+}
+Duration duration_from_micros(u64 micros)
+{
+    return (Duration){SDL_US_TO_NS(micros)};
+}
+
+f32 duration_as_secs_f32(Duration duration)
+{
+    return SDL_NS_TO_SECONDS((f32)duration.nanos);
+}
+f64 duration_as_secs_f64(Duration duration)
+{
+    return SDL_NS_TO_SECONDS((f64)duration.nanos);
+}
+
+Instant instant_now(void) { return (Instant){SDL_GetTicksNS()}; }
+
+Duration instant_elapsed(Instant instant)
+{
+    Instant now = instant_now();
+    return instant_duration_since(now, instant);
+}
+Duration instant_duration_since(Instant instant, Instant earlier)
+{
+    return instant_sub(instant, earlier);
+}
+
+Duration instant_sub(Instant instant, Instant earlier)
+{
+    return (Duration){instant.dur.nanos - earlier.dur.nanos};
+}
+Instant instant_sub_dur(Instant instant, Duration time)
+{
+    return (Instant){instant.dur.nanos - time.nanos};
+}
+Instant instant_add(Instant instant, Duration time)
+{
+    return (Instant){instant.dur.nanos + time.nanos};
+}
---
+++ src/utility/time.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "sensible_nums.h"
+
+// Somewhat inspired by Rust's std::time::Duration, this represents a span of
+// time.
+// Durations are represented as nanoseconds.
+typedef struct
+{
+    // this field is not intended to be used directly!
+    u64 nanos;
+} Duration;
+
+Duration duration_new(u64 nanos);
+Duration duration_from_secs(u64 secs);
+Duration duration_from_millis(u64 millis);
+Duration duration_from_micros(u64 micros);
+
+f32 duration_as_secs_f32(Duration duration);
+f64 duration_as_secs_f64(Duration duration);
+
+// Instants are inspired by Rust's std::time::Instant, representing a fixed
+// point in time. Barring platform bugs the should be nondecreasing.
+typedef struct
+{
+    // this field is not intended to be used directly!
+    Duration dur;
+} Instant;
+
+Instant instant_now(void);
+
+Duration instant_elapsed(Instant instant);
+Duration instant_duration_since(Instant instant, Instant earlier);
+
+Duration instant_sub(Instant instant, Instant earlier);
+Instant instant_sub_dur(Instant instant, Duration time);
+Instant instant_add(Instant instant, Duration time);
