663c7ee4f036c9f0cd104cf99d54abb01ecbda61
callistoAshley <callistoashley@proton.me>
2024-10-19 21:59:06 +1100

    programmer art

---
+++ assets/tilesets/tileset_test2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tileset_test2" tilewidth="8" tileheight="8" tilecount="2" columns="2">
+ <image source="../textures/tileset_test2.png" width="16" height="8"/>
+</tileset>
