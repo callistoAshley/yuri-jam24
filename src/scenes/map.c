d311981c69263d7aa6bc3e2cb8b76f8405d5826d
Melody Madeline Lyons <melody@melody-is.gay>
2024-11-03 01:38:06 -0800

    remove debug bgm

--- src/scenes/map.c
+++ src/scenes/map.c
@@ -1,5 +1,4 @@
 #include "map.h"
-#include "fmod_studio.h"
 #include "input/input.h"
 #include "player.h"
 #include "scenes/scene.h"
@@ -63,18 +62,6 @@ void map_scene_init(Scene **scene_data, Resources *resources, void *extra_args)
     vec_init(&map_scene->colliders, sizeof(b2BodyId));
     vec_init(&map_scene->renderables, sizeof(MapRenderable));
     vec_init(&map_scene->characters, sizeof(MapCharacterEntry));
-
-    FMOD_RESULT result;
-    if (!resources->audio->current_bgm)
-    {
-        FMOD_STUDIO_EVENTDESCRIPTION *desc;
-        result = FMOD_Studio_System_GetEvent(resources->audio->system,
-                                             "event:/bgm/morning", &desc);
-        FMOD_ERRCHK(result, "Failed to get event");
-        FMOD_Studio_EventDescription_CreateInstance(
-            desc, &resources->audio->current_bgm);
-        FMOD_Studio_EventInstance_Start(resources->audio->current_bgm);
-    }
 
     map_scene->freecam = false;
 
