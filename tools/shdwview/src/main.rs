6dc002ce0bdc78d623a27484ebe07067ad795ec1
Melody Madeline Lyons <melody@melody-is.gay>
2024-11-19 23:09:50 -0800

    add shdw viewer

--- tools/shdwview/src/main.rs
+++ tools/shdwview/src/main.rs
@@ -7,14 +7,14 @@ struct App {
     texture_handle: egui::TextureHandle,
 
     shdw: Shdw,
-    shdw_file: std::fs::File,
 }
 
+/// Views a shdw file against a reference image.
 #[derive(clap::Parser)]
 struct Args {
     /// The path to the reference image
     image: String,
-    /// The path to the output shdw file
+    /// The path to the shdw file
     shdw: String,
 }
 
@@ -102,14 +102,15 @@ type Error = Box<dyn std::error::Error + Send + Sync>;
 
 impl App {
     fn new(args: Args, cc: &eframe::CreationContext<'_>) -> Result<Self, Error> {
-        let image = image::open(args.image)?.into_rgba8();
+        let mut image = image::open(args.image)?.into_rgba8();
+        // discard any transparent pixels (egui renders them weird)
+        for pixel in image.pixels_mut() {
+            if pixel.0[3] == 0 {
+                pixel.0 = [0; 4];
+            }
+        }
 
-        let mut shdw_file = std::fs::File::options()
-            .create(true)
-            .truncate(false)
-            .read(true)
-            .write(true)
-            .open(args.shdw)?;
+        let mut shdw_file = std::fs::File::open(args.shdw)?;
         let shdw = Shdw::read(&mut shdw_file).unwrap_or_else(|err| {
             eprintln!("Failed to load shdw: {err}");
             eprintln!("Using default instead");
@@ -128,28 +129,27 @@ impl App {
             texture_handle,
 
             shdw,
-            shdw_file,
         })
     }
 }
 
 impl eframe::App for App {
-    fn update(&mut self, ctx: &eframe::egui::Context, frame: &mut eframe::Frame) {
+    fn update(&mut self, ctx: &eframe::egui::Context, _: &mut eframe::Frame) {
+        let [texture_width, texture_height] = self.texture_handle.size();
         egui::TopBottomPanel::top("top menubar").show(ctx, |ui| {
             ui.horizontal(|ui| {
                 ui.label("Cell width");
                 egui::DragValue::new(&mut self.shdw.cell_width)
-                    .range(0..=100)
+                    .range(1..=texture_width)
                     .ui(ui);
 
                 ui.label("Cell height");
                 egui::DragValue::new(&mut self.shdw.cell_height)
-                    .range(0..=100)
+                    .range(1..=texture_height)
                     .ui(ui);
             });
         });
 
-        let [texture_width, texture_height] = self.texture_handle.size();
         let cell_count_x = texture_width / self.shdw.cell_width;
         let cell_count_y = texture_height / self.shdw.cell_height;
 
@@ -169,7 +169,6 @@ impl eframe::App for App {
                         image_center,
                         texture_size,
                     );
-                    let image = image.bg_fill(Color32::GRAY);
                     plot.image(image);
 
                     for (i, cell) in self.shdw.cells.iter().enumerate() {
@@ -178,6 +177,24 @@ impl eframe::App for App {
 
                         let cell_y = i / cell_count_x * self.shdw.cell_height;
                         let cell_y = cell_y as f64;
+
+                        let cell_w = self.shdw.cell_width as f64;
+                        let cell_h = self.shdw.cell_height as f64;
+
+                        let cell_points = [
+                            [cell_x, -cell_y],
+                            [cell_x + cell_w, -cell_y],
+                            [cell_x + cell_w, -(cell_y + cell_h)],
+                            [cell_x, -(cell_y + cell_h)],
+                        ];
+                        let cell_points = cell_points.into_iter().collect::<PlotPoints>();
+
+                        let cell_stroke = egui::Stroke::new(2.0, Color32::LIGHT_RED);
+                        let polygon = egui_plot::Polygon::new(cell_points)
+                            .fill_color(Color32::TRANSPARENT)
+                            .style(egui_plot::LineStyle::dashed_loose())
+                            .stroke(cell_stroke);
+                        plot.polygon(polygon);
 
                         let origins = cell
                             .lines
@@ -198,7 +215,8 @@ impl eframe::App for App {
                             })
                             .collect::<PlotPoints>();
 
-                        let arrows = egui_plot::Arrows::new(origins, tips);
+                        let arrows =
+                            egui_plot::Arrows::new(origins, tips).color(Color32::DEBUG_COLOR);
                         plot.arrows(arrows);
                     }
                 });
